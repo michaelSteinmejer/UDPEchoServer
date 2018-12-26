a2198b6ea0f3ad982a2b59370d4512e7bc5c0c59
michaelSteinmejer <33892912+michaelSteinmejer@users.noreply.github.com>
2018-12-26 12:38:59 +0100

    Initial commit

---
+++ UDPEchoServer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPEchoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates a UdpClient for reading incoming data.
+            UdpClient udpServer = new UdpClient(9999);
+
+            //Creates an IPEndPoint to record the IP Address and port number of the sender.  
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(ip, 9999);
+
+            try
+            {
+                // Blocks until a message is received on this socket from a remote host (a client).
+                Console.WriteLine("Server is blocked");
+                while (true)
+                {
+                    Byte[] receiveBytes = udpServer.Receive(ref RemoteIpEndPoint);
+                    //Server is now activated");
+
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+                    string[] data = receivedData.Split(' ');
+                    string clientName = data[0];
+                    string text = data[1] + data[2];
+
+                    Console.WriteLine(receivedData);
+                    //Console.WriteLine("Received from: " + clientName.ToString() + " " + text.ToString());
+
+                    string sendData = "Server: " + text.ToUpper();
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(sendData);
+
+                    udpServer.Send(sendBytes, sendBytes.Length, RemoteIpEndPoint);
+
+                    Console.WriteLine("This message was sent from " +
+                                      RemoteIpEndPoint.Address.ToString() +
+                                      " on their port number " +
+                                      RemoteIpEndPoint.Port.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
